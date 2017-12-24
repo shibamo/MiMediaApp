e82c33d406e2bd429acfe8db3aa0e3ef771e1533
shibamo <qinchao@hotmail.com>
2017-12-23 18:58:25 -0900

    增加重置密码问题集的英文版本

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -26,13 +26,93 @@ export class SignupPage {
   } = {};
   submitted = false;
 
+  quesitons = [];
+
+  static allQuestions : Object = {
+    cn: [
+      {
+        val: "您最喜欢的电视剧?",
+        text: "您最喜欢的电视剧?"
+      },
+      {
+        val: "您最喜欢的电影?",
+        text: "您最喜欢的电影?"
+      },
+      {
+        val: "您最喜欢的影视明星?",
+        text: "您最喜欢的影视明星?"
+      },
+      {
+        val: "您最喜欢的饭店名?",
+        text: "您最喜欢的饭店名?"
+      },
+      {
+        val: "您最喜欢的食物名?",
+        text: "您最喜欢的食物名?"
+      },
+      {
+        val: "您最喜欢的旅游地点?",
+        text: "您最喜欢的旅游地点?"
+      },
+      {
+        val: "您母亲的出生城市?",
+        text: "您母亲的出生城市?"
+      },
+      {
+        val: "您父亲的出生城市?",
+        text: "您父亲的出生城市?"
+      },                                     
+    ],
+    en: [
+      {
+        val: "Your favorite Tv series?",
+        text: "Your favorite Tv series?"
+      },
+      {
+        val: "Your favorite movie?",
+        text: "Your favorite movie?"
+      },
+      {
+        val: "Your favorite movie star?",
+        text: "Your favorite movie star?"
+      },
+      {
+        val: "Your favorite restaurant?",
+        text: "Your favorite restaurant?"
+      },
+      {
+        val: "Your favorite food?",
+        text: "Your favorite food?"
+      },
+      {
+        val: "Your favorite tourism area?",
+        text: "Your favorite tourism area?"
+      },
+      {
+        val: "Birth place of your mother?",
+        text: "Birth place of your mother?"
+      },
+      {
+        val: "Birth place of your father?",
+        text: "Birth place of your father?"
+      },                                     
+    ],
+  };
+  
   constructor(public navCtrl: NavController,
     public viewCtrl: ViewController,
     public toastCtrl: ToastController,
     public modalController: ModalController,
     public translateService: TranslateService,
     public userData: UserData,
-    public userService: UserService) {}
+    public userService: UserService) {
+      // 根据当前语言选择不同语言的重置密码问题数组
+      this.quesitons =  SignupPage.allQuestions[
+        translateService.currentLang ? 
+        translateService.currentLang :
+        translateService.defaultLang
+      ];
+    }
 
   onSignup(form: NgForm) {
     this.submitted = true;
